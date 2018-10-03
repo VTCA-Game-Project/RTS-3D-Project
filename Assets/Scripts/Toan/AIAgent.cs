f78569c3f3fda2e97972a818ba3e971f0ab77ce8
Nguyễn ĐinhToàn <ndinhtoan48@gmail.com>
2018-10-04 02:03:21 +0700

    AI and Contruct
    Unlock construct when new construct is buided. Complete.
    AI - Flocking : is edting properties

---
+++ Assets/Scripts/Toan/AIAgent.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AI;
+using Manager;
+
+namespace Agent
+{
+    public class AIAgent : MonoBehaviour
+    {
+        private SteerBehavior steerBh;
+        private FlockBehavior flockBh;
+
+        private Vector3 position;
+        private Vector3 heading;
+        private Vector3 steering;
+
+        private bool isSelected = false;
+        public Pointer target;
+        public Rigidbody rigid;
+        public float BoundRadius;
+        public float NeighbourRadius;
+
+        public float separation;
+        public float cohesion;
+        public float alignment;
+        public float maxSpeed;
+        public int index;
+        public AIAgent[] neighbours;
+        [Header("Debug")]
+        public bool drawGizmos = true;
+
+        #region Properties
+        public float MaxSpeed
+        {
+            get { return maxSpeed; }
+            protected set { maxSpeed = value; }
+        }
+        public Vector3 Position
+        {
+            get
+            {
+                position = transform.position;
+                position.y = 0;
+                return position;
+            }
+        }
+        public Vector3 Heading
+        {
+            get
+            {
+                heading = transform.forward;
+                heading.y = 0;
+                heading = heading.normalized;
+                return heading;
+            }
+        }
+        public bool IsSelected
+        {
+            get { return isSelected; }
+        }
+        #endregion
+
+        private void Awake()
+        {
+            StoredManager.AddAgent(this);
+        }
+        private void Start()
+        {
+            steerBh = AIUtils.steerBehaviorInstance;
+            flockBh = AIUtils.flockBehaviorInstance;
+        }
+        private void FixedUpdate()
+        {
+            steering = Vector3.zero;
+            neighbours = StoredManager.GetNeighbours(this);
+            //steering += steerBh.Seek(this, target.Position);
+            steering += steerBh.Arrive(this, target.Position);
+            steering += flockBh.Separation(this, neighbours) * separation;
+            steering += flockBh.Alignment(this, neighbours) * alignment;
+            steering += flockBh.Cohesion(this, neighbours) * cohesion;
+
+            rigid.velocity += steering / rigid.mass;
+            transform.forward +=  steering / rigid.mass;;
+#if UNITY_EDITOR
+            Debug.Log("steer: " + steering + " velocity: " + rigid.velocity );
+#endif
+        }
+
+        public void Select() { isSelected = true; }
+        public void UnSelect() { isSelected = false; }
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            if (drawGizmos)
+            {
+                Gizmos.color = Color.blue;
+                Gizmos.DrawWireSphere(transform.position, NeighbourRadius);
+            }
+        }
+#endif
+    }
+}
---
+++ Assets/Scripts/Toan/Pointer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pointer : MonoBehaviour
+{
+
+    private Vector3 position;
+
+    [SerializeField]
+    private Camera rtsCamera;
+
+    #region Properties
+    public Vector3 Position
+    {
+        get
+        {
+            position = transform.position;
+            return position;
+        }
+        set
+        {
+            value.y = 0;
+            transform.position = value;
+
+        }
+    }
+    #endregion
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = rtsCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(ray: ray,
+                hitInfo: out hitInfo,
+                maxDistance: 100.0f,
+                layerMask: LayerMask.GetMask("Place")))
+            {
+                Position = hitInfo.point;
+            }
+        }
+
+    }
+}
